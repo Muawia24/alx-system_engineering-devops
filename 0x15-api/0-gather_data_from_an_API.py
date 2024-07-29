9c2cbfc898a2b9f7ebefc7a226ad148eafbda876
Ahmed Muawia <amuawia666@gmail.com>
2024-07-29 23:54:30 +0300

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Gather data from an API """
+
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    tot_tasks = 0
+    done_tasks = 0
+
+    emp_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/todos/{}".format(emp_id)
+    employee = requests.get(url)
+
+    name = employee.json().get('name')
+
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    for task in todos.json():
+        if(task.get('userId') == int(emp_id)):
+            tot_tasks += 1
+            if(task.get('completed')):
+                done_tasks += 1
+
+    print("Employe {} is done with tasks({}/{}):".format(
+        name, done_tasks, tot_tasks))
+
+    for task in todos.json():
+        if (task.get('userId') == int(emp_id)):
+            if (task.get('completed')):
+                print(task.get('title'))
