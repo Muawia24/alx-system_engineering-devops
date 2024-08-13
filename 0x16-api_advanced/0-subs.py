984aca07304855f722965006f74aaa16833f2488
Ahmed Muawia <amuawia666@gmail.com>
2024-08-13 17:04:30 +0300

    Fixed the documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""0. How many subs? """
+"""0. How many subs?"""
 
 
 def number_of_subscribers(subreddit):
-    """
-    A function that queries the Reddit API and
-    returns the number of subscribers
-    """
+    """A function that queries the Reddit API and
+    returns the number of subscribers"""
     import requests
 
     headers = {'User-Agent': 'My-User-Agent'}
