62814a7483caf3b982887826e9bd5b01e455fdb0
Ahmed Muawia <amuawia666@gmail.com>
2024-08-14 17:16:10 +0300

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Using strace, will find out why Apache is returning a 500 error
+exec{'debug_error':
+command => 'sudo sed -i s/pphp/php/g /var/www/html/wp-settings.php: sudo service apache2 restart',
+path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
