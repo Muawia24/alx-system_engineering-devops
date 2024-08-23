3fc9a6604b16531477bd58b0f83a6b2f8a9494e4
Ahmed Muawia <amuawia666@gmail.com>
2024-08-23 18:08:28 +0300

    puppet-lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # Puppet script to fix "too many files user login error" 
 exec {'open-file-limit':
-command => 'sed -i "s/nofile [0-9]\+/nofile 5000/" /etc/security/limits.conf',
+command  => 'sed -i "s/nofile [0-9]\+/nofile 5000/" /etc/security/limits.conf',
 provider => shell
 }
