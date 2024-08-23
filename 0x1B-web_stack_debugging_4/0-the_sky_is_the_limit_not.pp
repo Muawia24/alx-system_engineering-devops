0cd6e647f5d380ccd4d434028f7128f831226078
Ahmed Muawia <amuawia666@gmail.com>
2024-08-23 14:48:47 +0300

    nginx config

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,6 +2,6 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-command => 'sed -i "s/ULIMIT=\"-n 4096\"/ULIMIT=\"-n 10000\"/" /etc/default/nginx && sudo service nginx restart',
+command => 'sed -i "s/worker_connections [0-9]\+/worker_connections 100/" /etc/nginx/nginx.conf && sudo service nginx restart',
 path    => '/usr/local/bin/:/bin/'
 }
