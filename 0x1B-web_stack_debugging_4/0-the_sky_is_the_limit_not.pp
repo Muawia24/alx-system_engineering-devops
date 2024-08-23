413afcb089dd9c82cf420c2e9ac7598ddd90cfbf
Ahmed Muawia <amuawia666@gmail.com>
2024-08-23 16:08:03 +0300

    Puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,6 +2,6 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--requests-failure':
-command => 'sed -i "s/15/4096/" /etc/default/nginx; sudo service nginx restart',
+command  => 'sed -i "s/15/4096/" /etc/default/nginx; sudo service nginx restart',
 provider => shell
 }
