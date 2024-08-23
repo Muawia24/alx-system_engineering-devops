b89456d47122f2c65e3e46a705679e265e0be735
Ahmed Muawia <amuawia666@gmail.com>
2024-08-23 14:34:43 +0300

    ulimit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,6 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-command => 'sed -i "s/15/4096/" /etc/default/nginx',
+command => 'sed -i "s/ULIMIT=\"-n 4096\"/ULIMIT=\"-n 10000\"/" /etc/default/nginx && sudo service nginx restart',
 path    => '/usr/local/bin/:/bin/'
-}
-
-exec {'nginx restart':
-command => 'sudo service nginx restart',
-path    => '/etc/init.d/'
 }
